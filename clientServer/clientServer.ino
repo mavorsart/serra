0bffd085133ce9fc2b53e42356cd3fd56538256c
Marco Sartini <marcosartini.ms@gmail.com>
2013-02-01 22:13:16 +0100

    Inzio sviluppo

---
+++ clientServer/clientServer.ino
@@ -0,0 +1,146 @@
+//zoomkat 12-08-11, combined client and server
+//simple button GET with iframe code
+//for use with IDE 1.0
+//open serial monitor and send an g to test client and
+//see what the arduino client/server receives
+//web page buttons make pin 4 high/low
+//use the \ slash to escape the " in the html 
+//address will look like http://192.168.1.102:84 when submited
+//for use with W5100 based ethernet shields
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //physical mac address
+IPAddress ip(192,168,1,102); // ip in lan
+IPAddress gateway(192,168,1,254); // internet access via router
+IPAddress subnet(255,255,255,0); //subnet mask
+IPAddress myserver(208,104,2,86); // zoomkat web page
+EthernetServer server(84); //server port
+EthernetClient client;
+String readString; 
+
+//////////////////////
+
+void setup(){
+
+  pinMode(13, OUTPUT); //pin selected to control
+  Ethernet.begin(mac, ip, subnet, gateway); 
+  server.begin();
+  Serial.begin(9600); 
+  Serial.println("server/client 1.0 test 12/08/11"); // keep track of what is loaded
+  Serial.println("Send an g in serial monitor to test client"); // what to do to test client
+}
+
+void loop(){
+  // check for serial input
+  if (Serial.available() > 0) 
+  {
+    byte inChar;
+    inChar = Serial.read();
+    if(inChar == 'g')
+    {
+      sendGET(); // call sendGET function
+    }
+  }  
+
+  EthernetClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        //read char by char HTTP request
+        if (readString.length() < 100) {
+
+          //store characters to string 
+          readString += c; 
+          //Serial.print(c);
+        } 
+
+        //if HTTP request has ended
+        if (c == '\n') {
+
+          ///////////////
+          Serial.println(readString); //print to serial monitor for debuging 
+
+            //now output HTML data header
+          if(readString.indexOf('?') >=0) { //don't send new page
+            client.println("HTTP/1.1 204 Zoomkat");
+            client.println();
+            client.println();  
+          }
+          else {
+            client.println("HTTP/1.1 200 OK"); //send new page
+            client.println("Content-Type: text/html");
+            client.println();
+
+            client.println("<HTML>");
+            client.println("<HEAD>");
+            client.println("<TITLE>Arduino GET test page</TITLE>");
+            client.println("</HEAD>");
+            client.println("<BODY>");
+
+            client.println("<H1>Zoomkat's simple Arduino 1.0 button</H1>");
+
+            client.println("<a href=\"/?on\" target=\"inlineframe\">ON</a>"); 
+            client.println("<a href=\"/?off\" target=\"inlineframe\">OFF</a>"); 
+
+            //client.println("<IFRAME name=inlineframe src=\"res://D:/WINDOWS/dnserror.htm\" width=1 height=1\">");
+            client.println("<IFRAME name=inlineframe style=\"display:none\" >");          
+            client.println("</IFRAME>");
+
+            client.println("</BODY>");
+            client.println("</HTML>");
+          }
+
+          delay(1);
+          //stopping client
+          client.stop();
+
+          ///////////////////// control arduino pin
+          if(readString.indexOf("on") >0)//checks for on
+          {
+            digitalWrite(13, HIGH);    // set pin 4 high
+            Serial.println("Led On");
+          }
+          if(readString.indexOf("off") >0)//checks for off
+          {
+            digitalWrite(13, LOW);    // set pin 4 low
+            Serial.println("Led Off");
+          }
+          //clearing string for next read
+          readString="";
+
+        }
+      }
+    }
+  }
+} 
+
+//////////////////////////
+void sendGET() //client function to send/receie GET request data.
+{
+  if (client.connect(myserver, 80)) {
+    Serial.println("connected");
+    client.println("GET /~shb/arduino.txt HTTP/1.0");
+    client.println();
+  } 
+  else {
+    Serial.println("connection failed");
+    Serial.println();
+  }
+
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available()) { //connected or data available
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  Serial.println();
+  Serial.println("disconnecting.");
+  Serial.println("==================");
+  Serial.println();
+  client.stop();
+
+}
--- serra/serra.ino
+++ serra/serra.ino
@@ -2,57 +2,99 @@
 #include <stdio.h>
 #include <string.h>
 #include <DS1302.h>
+//#include <Ethernet.h>
 
+// *** SENSORE TEMPERATURA 
+    #define DHTPIN 2     // what pin we're connected to
+    
+    #define DHTTYPE DHT22   // DHT 22  (AM2302)
+    
+    // Connect pin 1 (on the left) of the sensor to +5V
+    // Connect pin 2 of the sensor to whatever your DHTPIN is
+    // Connect pin 4 (on the right) of the sensor to GROUND
+    // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+    
+    DHT dht(DHTPIN, DHTTYPE);
+    float h,t;
+// *** FINE SENSORE TEMPERATURA
 
-#define DHTPIN 2     // what pin we're connected to
+// *** DISPOSITIVO RTC
+  // Set the appropriate digital I/O pin connections
+    uint8_t CE_PIN   = 5;
+    uint8_t IO_PIN   = 6;
+    uint8_t SCLK_PIN = 7;
+    
+   // Create a DS1302 object
+      DS1302 rtc(CE_PIN, IO_PIN, SCLK_PIN);
+      Time time;
+// *** FINE DISPOSITIVO RTC
 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+// *** IRRIGAZIONE
+  // dato un vettore ordinato di irrigazioni, l'ora corrente è quella che deve essere effettuata. Tipo "sveglia" in Assembly
+    Time corrente;
+// *** FINE IRRIGAZIONE
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+#define VENTOLA_1 22 //pin della ventola 1
+#define VENTOLA_2 23 //pin della ventola 2
+//#define OROLOGIO 24 //pin dell'RTC
+#define UMIDIFICATORE 25 //pin dell'umidificatore
+#define POMPA 26//pin della pompa (o del relè associato)
+#define RISCALDAMENTO 27 //pin della resistenza riscaldante (o relè associato)
+#define LIVELLO 28//pin del sensore di livello serbatoio
 
-DHT dht(DHTPIN, DHTTYPE);
-float h,t;
-
-#define VENTOLA 3;
-#define PL 4;
-#define T_AMB 20;
-#define H_MAX 70;
-#define H_MIN 30;
-#define OROLOGIO 4;
+float tAmb=20;
+float hMax=70;
+float hMin=30;
 
 
 void setup() {
   Serial.begin(9600);  
   dht.begin();
+  
 }
 
 void loop() {
   
+  //### legge la temnperatura dell'ambiente
+  
    h = dht.readHumidity();
    t = dht.readTemperature();
    
-   if(h>H_MAX || t > T_AMB){
-      digital.write(VENTOLA,HIGH);
-      pausaLavoro(LOW);
-   }
-      
-   if(t<=T_AMB)
-     digital.write(VENTOLA,LOW);
+   //### valuta come agire per stabilizzare il microclima
+           if(h>hMax || t > tAmb){
+              digitalWrite(VENTOLA_1,HIGH);
+              digitalWrite(VENTOLA_2,HIGH);
+              digitalWrite(UMIDIFICATORE,LOW);
+           }
+              
+           if(t<=tAmb){
+             digitalWrite(VENTOLA_1,LOW);
+             digitalWrite(VENTOLA_2,LOW);
+           }
+           if(t+3<=tAmb){
+             digitalWrite(VENTOLA_1,LOW);
+             digitalWrite(VENTOLA_2,LOW);
+             digitalWrite(RISCALDAMENTO,HIGH);
+           }
+             
+           if(h<hMin){
+               digitalWrite(UMIDIFICATORE,HIGH);
+               digitalWrite(VENTOLA_1,LOW);
+               digitalWrite(VENTOLA_2,LOW);
+           }
+   
+   //### invia al sito dell'applicazione le variazioni dell'ambiente
+       
+   //### legge l'ora attuale dal dispositivo RTC e controlla la schedulazione delle irrigazioni
+        time = rtc.time();
+       if(time==corrente && digitalRead(POMPA)){
+         digitalWrite(POMPA,HIGH);
+        // corrente=irrigazione[i].spegni;
+       }
+       if(time==corrente && !digitalRead(POMPA)){
+         digitalWrite(POMPA,LOW);
+        // corrente=irrigazione[++i].accendi;
+       }
+       
      
-   if(h<H_MIN)
-       pausaLavoro(HIGH);
-   
-   delay(5000);
-  }
-  
- void pausaLavoro(int stato){
-   if(stato==HIGH)
-   digital.write(PL,HIGH);
-   else
-   digital.write(PL,LOW);
-   
- }
-}
+} 
