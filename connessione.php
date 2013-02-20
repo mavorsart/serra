d6486392398616965c497a69838d8927763fb790
Marco Sartini <marcosartini.ms@gmail.com>
2013-02-20 23:28:44 +0100

    Connessione al DB

---
+++ connessione.php
@@ -0,0 +1,7 @@
+
+<?php
+//Connessione DB
+$connessione=mysql_connect("localhost","serrarduino","");
+mysql_select_db("my_serrarduino") or die ("Errore di connessione database: DB non trovato");
+
+?>
