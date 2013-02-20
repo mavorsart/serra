644ed2cf195fabdd6a8dcf87a4fab8ea86a65573
Marco Sartini <marcosartini.ms@gmail.com>
2013-02-20 23:30:13 +0100

    Librerie per la gestione delle irrigazioni

---
+++ irrigazione/irr.h
@@ -0,0 +1,22 @@
+/*
+irr.h
+*/
+#ifndef irr_included
+#define irr_included
+
+#include <Arduino.h>
+#include <Print.h> //the Arduino print class
+#include <Time.h>//o anche ds1302
+
+class irrClass : public Print
+{
+ private:
+  Time orarioAvvio;
+  Time orarioFine;
+ public:
+  irrClass();
+  boolean begin(Time t1,Time t2); 
+};
+
+extern irrClass irr; //the irr object
+#endif
